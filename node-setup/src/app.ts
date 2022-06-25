5cc20c35f5eda00fba9d26501fd5d37bc629200b
emadmhmd <emad.mohamedeng@gmail.com>
2022-06-25 11:28:34 +0200

    Clear app.ts

--- node-setup/src/app.ts
+++ node-setup/src/app.ts
@@ -1,11 +1,10 @@
 import express, { Request, Response } from 'express';
 import path from 'path';
-// import { Sequelize } from 'sequelize';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import indexRouter from './routes/user.route';
-import mongoDBConfig from '../config/mongoDB.config';
+import { userRoute } from './route';
+import { mongoDBConfig, postgresDBConfig } from '../config';
 
 const app = express();
 app.use(cors());
@@ -20,51 +19,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// const dbConnect = new Sequelize(
-//   'indexima',
-//   'root',
-//   'corelia',
-//   {
-//     host: '10.198.0.3',
-//     dialect: 'mariadb',
-//   },
-// );
-
-// dbConnect.authenticate().then(() => {
-//   console.log('Connection has been established successfully.');
-// }).catch((error) => {
-//   console.error('Unable to connect to the database:', error);
-// });
-
-// app.use('/', async () => {
-//   try {
-//     await dbConnect.authenticate();
-//     console.log('Connection has been established successfully.');
-//   } catch (error) {
-//     console.error('Unable to connect to the database:', error);
-//   }
-// });
-
-// const COCA_DB_URL = dbConfig.url;
-// console.log(COCA_DB_URL);
-
-// mongoose.connect(COCA_DB_URL, {
-//   useNewUrlParser: true,
-// });
-// mongoose.connection.on('connected', () => {
-//   console.log('Connected Successfully To Database ');
-// });
-// mongoose.connection.on('error', (err) => {
-//   console.error(`Failed To Database : ${err}`);
-// });
 mongoDBConfig();
+postgresDBConfig();
 
-app.use('/', indexRouter);
+app.use('/', userRoute);
 
 // catch 404 and forward to error handler
 app.use((req: Request, res: Response) => {
-  console.error('404 Page Not Found');
-  return res.send({
+  res.status(404).send({
     error: 'page not found',
   });
 });
