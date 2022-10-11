9c0252a96151874e45cb4c30bcc43011d4db0992
emadmhmd <emad.mohamedeng@gmail.com>
2022-10-11 09:45:20 +0200

    Import right type

--- node-setup/src/middleware/error.middleware.ts
+++ node-setup/src/middleware/error.middleware.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
-import { Error } from '../interface';
+import { ErrorType as Error } from '../types';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const errorMiddleware = async (err: Error, req: Request, res: Response, _next: NextFunction) => {
   const status = err.status || 500;
   const message = err.message || 'Whoops!! Something went wrong';
