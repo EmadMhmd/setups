382eb9ff63945eb4b73f9d8da88f3480b30a276e
emadmhmd <emad.mohamedeng@gmail.com>
2022-10-11 09:45:40 +0200

    Add auth middleware

--- node-setup/src/route/api/user.route.ts
+++ node-setup/src/route/api/user.route.ts
@@ -1,21 +1,23 @@
 import * as express from 'express';
 import { userController } from '../../controller';
+import isAuth from '../../middleware/auth.middleware';
+import isUserValid from '../../middleware/userValidation.middleware';
 
 const router = express.Router();
 
 router.route('/')
-  .post(userController.saveUser)
-  .get(userController.saveUser);
+  .all(isUserValid)
+  .post(userController.create);
 
-router.route('/:id')
-  .get(userController.getUserById)
-  .delete(userController.getUserById)
-  .patch(userController.getUserById)
-  .put(userController.getUserById);
+router.route('/login')
+  .all(isUserValid)
+  .post(userController.login);
 
-// router.route('/user/test')
-//   .get(async (req: express.Request, res:express.Response) => {
-//     res.send('Done');
-//   });
+router.route('/')
+  .all(isAuth)
+  .get(userController.get);
+// .delete(userController.getUserById)
+// .patch(userController.getUserById)
+// .put(userController.getUserById);
 
 export default router;
