fc72322ba8f350451af7ee960e8f9403b722bbe1
emadmhmd <emad.mohamedeng@gmail.com>
2022-10-11 09:38:04 +0200

    Export DB connect middleware

--- node-setup/config/index.ts
+++ node-setup/config/index.ts
@@ -2,9 +2,11 @@ import mongoDBConfig from './mongoDB.config';
 import postgresDBConfig from './postgresDB.config';
 import Env from './env.config';
 
-export {
-  mongoDBConfig,
-  postgresDBConfig,
+const connectToDB = () => {
+  mongoDBConfig();
+  postgresDBConfig();
 };
+
+export { connectToDB };
 
 export default Env;
